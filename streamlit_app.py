41a68372568603557bee96458f21a3a6c51efa73
miles <milesguidamorine@gmail.com>
2024-04-20 15:17:39 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,14 +5,15 @@ import base64
 # Function to create collapsible tree structure
 def create_tree_structure():
     nodes = [
-        {"label": "Folder A", "value": "folder_a"},
+        {"label": "how to make a youtube video",
+         "value": "folder_a"},
         {
             "label": "Folder B",
             "value": "folder_b",
             "children": [
                 {"label": "Sub-folder A", "value": "sub_a"},
                 {"label": "Sub-folder B", "value": "sub_b"},
-                {"label": "Sub-folder C", "value": "sub_c"},
+                {"label": "Sub-Folder C", "value": "sub_c"},
             ],
         },
         {
@@ -40,23 +41,23 @@ def download_file(file_name):
     # Replace with actual download logic
     st.write(f"Downloading {file_name}")
 
-
-def sidebar_bg(side_bg):
-    side_bg_ext = 'png'
-
-    st.markdown(
-      f"""
-      <style>
-      [data-testid="stSidebar"] > div:first-child {{
-          background: url(data:image/{side_bg_ext};base64,{base64.b64encode(open(side_bg, "rb").read()).decode()});
-      }}
-      </style>
-      """,
-      unsafe_allow_html=True,
-      )
-
 # Main function
 def main():
+
+    # Set the background image
+    background_image = """
+            <style>
+            [data-testid="stAppViewContainer"] > .main {
+                background-image: url("https://scontent.fyaw1-1.fna.fbcdn.net/v/t39.30808-6/280496183_397664592227372_7839417422075289256_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_ohc=vTrd1uIu3K0Ab6awg9q&_nc_ht=scontent.fyaw1-1.fna&oh=00_AfDCqGlDDi7ubQmBsn4t2oKHA6BUpAIX0SkqkBh558QKmA&oe=6629BA26");
+                background-size: 100vw 100vh;  # This sets the size to cover 100% of the viewport width and height
+                background-position: center;  
+                background-repeat: no-repeat;
+            }
+            </style>
+            """
+
+    st.markdown(background_image, unsafe_allow_html=True)
+
     st.title("Interactive Tree Web App")
 
     # Toggle for kids mode
@@ -64,28 +65,29 @@ def main():
 
     # Update background and add smiling clouds if kids mode is activated
     if kids_mode:
-        st.write("Kids mode activated!")
-        # sidebar_bg('./app/static/rainbow_nonsense.png')
-        # with st.echo():
-        st.markdown("[![Click me](./app/static/rainbow_nonsense.png)](https://streamlit.io)")
-        # st.markdown(
-        #     """
-        #     <style>
-        #     body {
-        #         background-image: url("./app/static/rainbow_nonsense.png");
-        #         background-size: cover;
-        #     }
-        #     </style>
-        #     """,
-        #     unsafe_allow_html=True,
-        # )
+        original_title = '<h1 style="font-family: sans-serif; color:black; font-size: 20px;">Kids mode activated!!✨ </h1>'
+        st.markdown(original_title, unsafe_allow_html=True)
+
+        # Set the background image
+        background_image = """
+        <style>
+        [data-testid="stAppViewContainer"] > .main {
+            background-image: url("https://p7.hiclipart.com/preview/975/564/23/rainbow-cloud-clip-art-rainbow-sun-painted-flowers.jpg");
+            background-size: 100vw 100vh;  # This sets the size to cover 100% of the viewport width and height
+            background-position: center;  
+            background-repeat: no-repeat;
+        }
+        </style>
+        """
+
+        st.markdown(background_image, unsafe_allow_html=True)
 
     # Search bar
     search_query = st.text_input("Search for files")
 
     # Create collapsible tree structure
     return_select = tree_select(create_tree_structure())
-    st.write(return_select)
+    # st.write(return_select)
 
     # Handle double-click events
     # if st.session_state.clicked_file:
